cc4dd6f4b0c8c7e8661366dcfc4cabe493770481
Felip7Dz <154281713+Felip7Dz@users.noreply.github.com>
2024-04-12 15:01:42 +0200

    Nuevo método deleteDataset

--- app.py
+++ app.py
@@ -300,7 +300,15 @@ def deleteDataset(user):
         filename_with_extension = request.json.get('nombre')
 
         filename_without_extension, _ = os.path.splitext(filename_with_extension)
-
+        
+        if (user == 'admin'):
+            tmp = filename_with_extension.split("(")
+            filename_without_extension = tmp[0].strip()
+            folder_path = 'prog_analizador/saved_models/' + tmp[1].replace(")", "").strip()
+            filename_with_extension = tmp[0].strip() + '.csv'
+            if not os.path.exists(os.path.join(folder_path, filename_with_extension)):
+                filename_with_extension = tmp[0].strip() + '.h5'
+        
         file_path = os.path.join(folder_path, filename_with_extension)
 
         if os.path.exists(file_path):
